5bf7d7425ff4bd5148ee403f2cd51e5cb2b49851
bryanliang1995 <1443741327@qq.com>
2019-08-12 13:34:11 +0800

    hello

---
+++ bnlearn.R
@@ -0,0 +1,20 @@
+install.packages("bnlearn")
+source("http://bioconductor.org/biocLite.R")
+biocLite(c("graph","Rgraphviz","RBGL"))
+install.packages("gRain")
+install.packages(c("pcalg","catnet","abn"))
+
+## construct a Discrete BNs ##
+
+asia.dag=model2network("[A][S][T|A][L|S][B|S][D|B:E][E|T:L][X|E]")
+lv=c("yes","no")
+A.prob=array(c(0.01,0.99),dim=2,dimnames = list(A=lv))
+S.prob=array(c(0.01,0.09),dim=2,dimnames = list(A=lv))
+T.prob=array(c(0.05,0.95,0.01,0.99),dim=c(2,2),dimnames = list(T=lv,A=lv))
+L.prob=array(c(0.1,0.9,0.01,0.99),dim=c(2,2),dimnames = list(L=lv,S=lv))
+B.prob=array(c(0.6,0.4,0.3,0.7),dim=c(2,2),dimnames = list(B=lv,S=lv))
+D.prob=array(c(0.9,0.1,0.7,0.3,0.8,0.2,0.1,0.9),dim=c(2,2,2),dimnames = list(D=lv,B=lv,E=lv))
+E.prob=array(c(1,0,1,0,1,0,0,1),dim=c(2,2,2),dimnames = list(E=lv,T=lv,L=lv))
+X.prob=array(c(0.98,0.02,0.05,0.95,dim=c(2,2),dimnames=list(X=lv,E=lv))
+cpt=list(A=A.prob,S=S.prob,T=T.prob,L=L.prob,B=B.prob,D=D.prob,E=E.prob,X=X.prob)
+bn=custom.fit(asia.dag,cpt)
